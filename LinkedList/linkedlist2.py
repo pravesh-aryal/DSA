e45e16b986fd7912fd7640fc61fb5708e8d4bf11
pravesh <pravesh.aryal696969@gmail.com>
2025-03-11 10:58:30 -0500

    linkedlist2.py deletion of node by reference added

--- LinkedList/linkedlist2.py
+++ LinkedList/linkedlist2.py
@@ -27,6 +27,9 @@ class LinkedList:
 
     def __str__(self):
         #shows what the current list looks like
+        if self.head == None:
+            return "The linkedlist is empty" 
+
         currentNode = self.head
         strFormat = ""
         while (currentNode != None):
@@ -46,14 +49,55 @@ class LinkedList:
         node.next = headNode
         self.head = node
 
+    
+    def deleteNodeByReference(self, node):
+        if (self.head == None):
+            self.__str__()
+            return
 
+        currentNode = self.head
+        #if node to delete is head.
+        if (node == self.head):
+            #set the second node as head.
+            self.head = self.head.next
+            print(self.__str__())
+            return
+        previousNode = None
+        while (currentNode != None):
+
+            if currentNode == node:
+                previousNode.next = currentNode.next
+            previousNode = currentNode
+            currentNode = currentNode.next
+
+        print(self.__str__())
+        
 linkedList1 = LinkedList()
+#
+#for i in range(0, 5):
+#    linkedList1.insertNodeAtBeginning(Node(i+1))
+#
+##the LL should handle creating nodes and setting up next's automatically.
+#for i in range(0, 10):
+#    linkedList1.insertNodeAtEnd(Node(i+1))
+#
 
-for i in range(0, 5):
-    linkedList1.insertNodeAtBeginning(Node(i+1))
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
 
-#the LL should handle creating nodes and setting up next's automatically.
-for i in range(0, 10):
-    linkedList1.insertNodeAtEnd(Node(i+1))
+linkedList1.insertNodeAtEnd(node1)
+linkedList1.insertNodeAtEnd(node2)
+linkedList1.insertNodeAtEnd(node3)
+linkedList1.insertNodeAtEnd(node4)
 
-print(linkedList1)
+linkedList1.deleteNodeByReference(node1)
+linkedList1.deleteNodeByReference(node2)
+linkedList1.deleteNodeByReference(node4)
+linkedList1.deleteNodeByReference(node3)
+linkedList1.deleteNodeByReference(node1)
+
+
+
+
