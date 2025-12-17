12cef04fac4956f7b88235c0256896428f339a49
Pravesh Aryal <pravesh@Mac.dhost.uta.edu>
2025-12-17 13:56:37 -0600

    Recursion: printing Subsequences

---
+++ striverPrintingSubsequence.py
@@ -0,0 +1,21 @@
+array = [3, 1, 2]
+length = len(array)
+
+#print all subsequences
+def printAllSubsequences(index, result, length):
+    if index >= length:
+        print(result)
+        return
+    
+    #choose the current index ell to proceed i.e left recursion 
+    result.append(array[index])
+    index += 1
+    printAllSubsequences(index, result, length)
+    #not choosing the current index ell to proceed i.e right recrusion
+    result.pop() #need to pop the last ell because it was added above i.e to not choose it
+    printAllSubsequences(index, result, length)
+
+index = 0
+result = []
+printAllSubsequences(index, result, length)
+
